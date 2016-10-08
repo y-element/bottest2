cdf51f7c417121ae19e541c4bb1709a545738237
y-element <ultravistor+21@gmail.com>
2016-10-08 13:24:27 +0900

    bugfix

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,7 @@ router.post('/webhook/', function (req, res) {
   const events = req.body.entry[0].messaging;
   for (i = 0; i < events.length; i++) {
     const event = req.body.entry[0].messaging[i];
+    const sender = event.sender.id;
     if (event.message && event.message.text) {
       const text = event.message.text;
       console.log(text+"2")
